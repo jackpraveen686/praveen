a3e0d4dde583afc69220243bfe7a1d1ca2e4a2fd
praveen badass <jackpraveen686@gmail.com>
2022-08-18 07:31:50 -0700

    the linked list

--- b.py
+++ b.py
@@ -1,4 +1,27 @@
-a=5
-b=6
-print('the sum of a and b is ',a+b)
-print('the name of the scientist is the glamour')
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty!")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data,end=" ")
+                n = n.ref
+ll=LinkedList()
+n1=Node(2)
+ll.head=n1
+n2=Node(3)
+n1.ref=n2
+n3=Node(4)
+n2.ref=n3
+ll.print_LL()
